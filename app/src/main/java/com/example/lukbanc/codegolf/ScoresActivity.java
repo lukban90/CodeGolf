f9cef1063a2b7ff1b98bb96a0210ad6be89acabb
Nico <salasn@student.wwu.edu>
2017-11-03 17:10:35 -0700

    activity scores layout done still needs some styling

---
+++ app/src/main/java/com/example/lukbanc/codegolf/ScoresActivity.java
@@ -0,0 +1,19 @@
+package com.example.lukbanc.codegolf;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class ScoresActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scores);
+    }
+
+    public void toPuzzles() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/example/lukbanc/codegolf/SolutionEditActivity.java
@@ -0,0 +1,13 @@
+package com.example.lukbanc.codegolf;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SolutionEditActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_solution_edit);
+    }
+}
