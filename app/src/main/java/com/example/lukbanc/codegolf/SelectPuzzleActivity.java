840c9fc9d06c557d521c19f1f62c2c339c9e0577
lukbanc <lukban90>
2017-11-07 12:03:42 -0800

    Added SelectPuzzle and puzzle_select -- Activity

--- app/src/main/java/com/example/lukbanc/codegolf/SelectPuzzleActivity.java
+++ app/src/main/java/com/example/lukbanc/codegolf/SelectPuzzleActivity.java
@@ -1,44 +1,66 @@
 package com.example.lukbanc.codegolf;
 
-import android.app.ActionBar;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.LinearLayout;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
+import android.widget.ScrollView;
 
 /**
  * Created by lukbanc on 11/2/17.
  */
 
 public class SelectPuzzleActivity extends AppCompatActivity {
-
-    private RadioGroup radioGroup;
+    private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_puzzle_select);
-
-        radioGroup = (RadioGroup)findViewById(R.id.radio_group);
-
-        // create a test puzzle, store it in the database
-        DatabaseManager dbm = new DatabaseManager(this);
-
-
+        upDateView();
     }
 
-    public void addRadioButtons(int number) {
+    public void upDateView() {
 
-        for(int i=0; i < number; i++) {
-            RadioButton radioButton = new RadioButton(this);
-            radioButton.setId(i + 1000);
-            radioButton.setText("RadioButton" + i);
-            LinearLayout.LayoutParams params =
-                    new LinearLayout.LayoutParams(ActionBar.LayoutParams.MATCH_PARENT,
-                                                  ActionBar.LayoutParams.WRAP_CONTENT, 1f);
-            radioButton.setLayoutParams(params);
-            radioGroup.addView(radioButton);
-        }
+        //Replace dummies with actual puzzles
+        String[] values = new String[] {"Chris is the best",
+                "Chirs is number one",
+                "Chirs is number one",
+                "Chirs is number one",
+                "Heroes for hire",
+                "Heroes for hire",
+                "Heroes for hire",
+                "Chirs is number one",
+                "Chirs is number one",
+                "Heroes for hire",
+                "Chirs is number one",
+                "Marvel buys Fox",
+                "Marvel buys Fox",
+                "Marvel buys Fox",
+                "Marvel buys Fox",
+                "Heroes for hire",
+                "Heroes for hire",
+                "Heroes for hire",
+                "Heroes for hire",
+                "Heroes for hire",
+                "Marvel buys Fox",
+                "Marvel buys Fox",
+                "Chirs is number one",
+                "Give me back FF4"};
+
+        ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(this,
+                R.layout.activity_listview_sample,
+                values);
+
+        listView = (ListView)findViewById(R.id.list_puzzle_select);
+        listView.setAdapter(myAdapter);
+    }
+
+    public void goBack(View v) {
+        this.finish();
     }
 }
