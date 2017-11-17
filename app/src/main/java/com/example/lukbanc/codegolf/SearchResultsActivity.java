6b2f75c8a2f1394fa2971301cff2804620cd9fdb
lukbanc <lukban90>
2017-11-17 08:55:47 -0800

    Added SelectPuzzle and puzzle_select -- Activity

--- app/src/main/java/com/example/lukbanc/codegolf/SearchResultsActivity.java
+++ app/src/main/java/com/example/lukbanc/codegolf/SearchResultsActivity.java
@@ -1,10 +1,17 @@
 package com.example.lukbanc.codegolf;
 
+import android.app.Activity;
 import android.app.SearchManager;
 import android.content.Intent;
+import android.inputmethodservice.Keyboard;
+import android.inputmethodservice.KeyboardView;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.InputType;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
 import android.widget.Toast;
 
 /**
@@ -12,6 +19,8 @@ import android.widget.Toast;
  */
 
 public class SearchResultsActivity extends AppCompatActivity {
+    private Keyboard keyboard;
+    private KeyboardView keyboardView;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -20,7 +29,122 @@ public class SearchResultsActivity extends AppCompatActivity {
         //add layout here
         setContentView(R.layout.activity_solution_edit);
         handleIntent(getIntent());
+
+        // Create the keyboard
+        keyboard = new Keyboard(this, R.xml.keyboard);
+        keyboardView = (KeyboardView)findViewById(R.id.keyboardview);
+        keyboardView.setKeyboard( keyboard );
+
+        //registerEditText(R.id.puzzle_soln);
+
+        //keyboardView.setOnKeyboardActionListener(myKeyboardActionListener);
     }
+
+    // onClick to open keyboard
+    public void openKeyoard(View v) {
+        keyboardView.setVisibility(View.VISIBLE);
+        keyboardView.setEnabled(true);
+        if(v != null) {
+            ((InputMethodManager) getSystemService(
+                    Activity.INPUT_METHOD_SERVICE)).hideSoftInputFromWindow(
+                    v.getWindowToken(), 0);
+
+            //registerEditText(R.id.puzzle_soln);
+        }
+    }
+
+    // register EditText to the keyboard
+    public void registerEditText(int resId) {
+        EditText editText = (EditText)findViewById(resId);
+
+        editText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean hasFocus) {
+                if(hasFocus)
+                    showCustomKeyboard(view);
+                else
+                    hideCustomKeyboard();
+            }
+        });
+        editText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showCustomKeyboard(view);
+            }
+        });
+        editText.setOnTouchListener(new View.OnTouchListener() {
+            @Override public boolean onTouch(View v, MotionEvent event) {
+                EditText edittext = (EditText) v;
+                int inType = edittext.getInputType();       // Backup the input type
+                edittext.setInputType(InputType.TYPE_NULL); // Disable standard keyboard
+                edittext.onTouchEvent(event);               // Call native handler
+                edittext.setInputType(inType);              // Restore input type
+                return true; // Consume touch event
+            }
+        });
+        // Disable spell check (hex strings look like words to Android)
+        editText.setInputType( editText.getInputType() | InputType.TYPE_TEXT_FLAG_NO_SUGGESTIONS );
+    }
+
+    // helpers to hide/show/check customboard
+    public void hideCustomKeyboard() {
+        keyboardView.setVisibility(View.GONE);
+        keyboardView.setEnabled(false);
+    }
+
+    public void showCustomKeyboard( View v ) {
+        keyboardView.setVisibility(View.VISIBLE);
+        keyboardView.setEnabled(true);
+        if( v!=null ) ((InputMethodManager)getSystemService(Activity.INPUT_METHOD_SERVICE)).hideSoftInputFromWindow(v.getWindowToken(), 0);
+    }
+
+    public boolean isCustomKeyboardVisible() {
+        return keyboardView.getVisibility() == View.VISIBLE;
+    }
+
+    // Functionality of the keyboard
+    private KeyboardView.OnKeyboardActionListener myKeyboardActionListener =
+            new KeyboardView.OnKeyboardActionListener() {
+                @Override
+                public void onPress(int i) {
+
+                }
+
+                @Override
+                public void onRelease(int i) {
+
+                }
+
+                @Override
+                public void onKey(int i, int[] ints) {
+
+                }
+
+                @Override
+                public void onText(CharSequence charSequence) {
+
+                }
+
+                @Override
+                public void swipeLeft() {
+
+                }
+
+                @Override
+                public void swipeRight() {
+
+                }
+
+                @Override
+                public void swipeDown() {
+
+                }
+
+                @Override
+                public void swipeUp() {
+
+                }
+            };
 
     public void updateView() {}
 
